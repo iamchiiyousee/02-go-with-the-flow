0a548fed695677b2dfe66b0b228f80b4ac516d45
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-06-15 10:36:15 -0400

    unfinished burger program

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Burger Program!");
+            Console.WriteLine("^_^");
+            Console.WriteLine("Would you like a burger today?");
+            String Burger = (Console.ReadLine());
+
+            if (Burger == "yes")
+            {
+                Console.WriteLine("Great, one burger. Would you like fries with that?");
+                String Fries = (Console.ReadLine()); 
+
+            if (Fries == "Yes")
+            {
+                Console.WriteLine("Fantastic! Okay, one more thing, would you like a drink with your meal?");
+                String Drink = (Console.ReadLine());
+
+            }
+
+            }
+
+
+
+            else
+            {
+                Console.WriteLine("That's not what I asked.");
+                Console.ReadLine();
+                Console.WriteLine("Would you like a burger today?");
+                Console.ReadLine();
+            }
+            if (Burger =="No")
+            {
+                Console.WriteLine("Okay, would you like anything else?");
+                Console.ReadLine();
+            }
+        }
+    }
+}
