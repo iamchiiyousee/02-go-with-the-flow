2dbeee355575114642f5805f5d533ff72a860c19
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-08-14 18:28:07 -0400

    added while loop

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -10,39 +10,43 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Burger Program!");
-            Console.WriteLine("^_^");
-            Console.WriteLine("Would you like a burger today?");
-            String answer = (Console.ReadLine());
-
-            if (answer.ToLower() == "yes")
+            while (true)
             {
-                Console.WriteLine("Great, one burger. Would you like fries with that?");
-                answer = (Console.ReadLine()); 
 
-            if (answer.ToLower() == "yes")
-            {
-                Console.WriteLine("Fantastic! Okay, one more thing, would you like a drink with your meal?");
-                answer = (Console.ReadLine());
-                    
-
-            }
-
-            }
-
-
-
-            else
-            {
-                Console.WriteLine("That's not what I asked.");
-                Console.ReadLine();
+                Console.WriteLine("Welcome to Burger Program!");
+                Console.WriteLine("^_^");
                 Console.WriteLine("Would you like a burger today?");
-                Console.ReadLine();
-            }
-            if (answer =="No")
-            {
-                Console.WriteLine("Okay, would you like anything else?");
-                Console.ReadLine();
+                String answer = (Console.ReadLine());
+
+                if (answer.ToLower() == "yes")
+                {
+                    Console.WriteLine("Great, one burger. Would you like fries with that?");
+                    answer = (Console.ReadLine());
+
+                    if (answer.ToLower() == "yes")
+                    {
+                        Console.WriteLine("Fantastic! Okay, one more thing, would you like a drink with your meal?");
+                        answer = (Console.ReadLine());
+
+
+                    }
+
+                }
+
+
+
+                else
+                {
+                    Console.WriteLine("That's not what I asked.");
+                    Console.ReadLine();
+                    Console.WriteLine("Would you like a burger today?");
+                    Console.ReadLine();
+                }
+                if (answer == "No")
+                {
+                    Console.WriteLine("Okay, would you like anything else?");
+                    Console.ReadLine();
+                }
             }
         }
     }
